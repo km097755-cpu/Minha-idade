e8be193aaf367542a75d7581d36b7bdc899dcdad
ProjetoGit <km097755@gmail.com>
2026-09-18 17:41:14 -0300

    Primeira versão do aplicativo

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+byte idade = 10;
+
+
+if (idade <=10)
+    System.out.println("Criança");
+else if (idade <+17)
+    } System.out.println("Adolescente");
+
+
+}
